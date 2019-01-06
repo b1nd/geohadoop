0e5b0fc39caccfee524469559d59e9699fa7f790
b1nd <h33el@yandex.ru>
2019-01-06 20:01:15 +0300

    init data-node

---
+++ data-node/src/main/java/ru/b1nd/datanode/DataNodeApp.java
@@ -0,0 +1,14 @@
+package ru.b1nd.datanode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataNodeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataNodeApp.class, args);
+	}
+
+}
+
