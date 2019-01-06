97f4ced504ead7543dafb971a3130b15c4c6ebb0
b1nd <h33el@yandex.ru>
2019-01-06 19:54:09 +0300

    init name-node

---
+++ name-node/src/main/java/ru/b1nd/namenode/NameNodeApp.java
@@ -0,0 +1,14 @@
+package ru.b1nd.namenode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameNodeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameNodeApp.class, args);
+	}
+
+}
+
